1295b2004fb339a834ab4c8b2fa3eba8f2448086
ronngo <ronngomn23@gmail.com>
2020-09-22 11:33:18 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AccessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.setColor("red");
+            circle.setRadius(2.0);
+            Console.WriteLine(circle.getColor());
+            Console.WriteLine(circle.getRadius());
+
+        }
+    }
+}
